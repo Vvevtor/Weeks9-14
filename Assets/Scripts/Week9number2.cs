a55acf961020c254abd636bf54c0a2fd2029d2c2
Felipe Sanchez Hancox <sanchefe@shernet.sheridancollege.ca>
2025-03-13 15:56:30 -0400

    first week 9 coding gym update, will finish later.

---
+++ Assets/Scripts/Week9number2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Week9number2 : MonoBehaviour
+{
+
+    public RectTransform baseshape; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
