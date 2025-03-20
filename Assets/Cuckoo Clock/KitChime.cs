0197f82e40ebdc4d36b332712490d26a5d14014c
Felipe Sanchez Hancox <sanchefe@shernet.sheridancollege.ca>
2025-03-20 15:38:11 -0400

    second upload, I'm gonna get that bird to pop out soon.

--- Assets/Cuckoo Clock/KitChime.cs
+++ Assets/Cuckoo Clock/KitChime.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class KitChime : MonoBehaviour
 {
     public KitClock clock;
+    private AudioSource chime;
 
     private void Start()
     {
         clock.OnTheHour.AddListener(Chime);
+        chime = GetComponent<AudioSource>();
     }
     public void Chime(int hour)
     {
         Debug.Log("Chiming " + hour + " o'clock!");
+        chime.Play();
+
     }
     public void ChimeWithoutArguments()
     {
         Debug.Log("Chiming !");
     }
+
+    //IEnumerator CuckooBird()
+    //{
+        
+    //}
 }
